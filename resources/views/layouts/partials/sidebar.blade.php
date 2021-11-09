2f19d04a2edc693cd8a2bc3bd87b455b2501273a
FitriaAmelia <fitriaamelia110@gmail.com>
2021-11-09 16:40:03 +0700

    konfigurasi role santigarcor

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -667,25 +667,8 @@
             <a href="#" class="nav-link">
               <i class="nav-icon far fa-circle text-info"></i>
               <p>Informational</p>
-            </a>
-          </li>
-         <li class="nav-item dropdown">
-           <a href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                 <p>logout</p>
-              </a>
-
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                     <a class="dropdown-item" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                              document.getElementById('logout-form').submit();">
-                                 {{ __('Logout') }}
-                                    </a>
-
-                                      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                       @csrf
-        </form>
-          </div>
-            </li>
+          </a>
+           </li>
             </ul>
              </nav>
         <!-- /.sidebar-menu -->
